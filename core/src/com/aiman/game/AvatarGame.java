7f2b09e09f80b34201d66de86dc6b5b14c8f0957
Mohd Aiman Jaffer <aiman.jaffer@gmail.com>
2020-07-20 23:58:50 +0530

    Using Viewport, setting the conversion rate of pixels to in-game distance units

--- core/src/com/aiman/game/AvatarGame.java
+++ core/src/com/aiman/game/AvatarGame.java
@@ -5,23 +5,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class AvatarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	ScreenViewport viewport;
+	public static float PPM = 16;
 	@Override
 	public void create () {
+		viewport = new ScreenViewport();
+		viewport.setUnitsPerPixel(1/PPM);
+		viewport.update(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1,0,0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(viewport.getCamera().combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, img.getWidth()/PPM,img.getHeight()/PPM);
 		batch.end();
 	}
 	
