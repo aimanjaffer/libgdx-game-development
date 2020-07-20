95aa7a77b0487f6201cad882c6afa1b926b570e4
Mohd Aiman Jaffer <aiman.jaffer@gmail.com>
2020-07-20 20:05:43 +0530

    initial commit

---
+++ desktop/src/com/aiman/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aiman.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.aiman.game.AvatarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new AvatarGame(), config);
+	}
+}
